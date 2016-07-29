1d897be638dfc91b8d27c22725562f3daa063a2b
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-07-29 11:07:28 +0800

    Added Branch and Position API

---
+++ app/Http/Controllers/Api/BranchApi.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Branch;
+
+class BranchApi extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(array(
+            'active_branches' => Branch::all()
+        ),
+            200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $branch          =   Branch::create(array(
+            'str_branch_location'   => $request->str_branch_location
+        ));
+        
+        return response()
+            ->json(
+                [
+                    'message'       =>  'Branch is successfully created.',
+                    'branch'        =>  $branch
+                ],
+                201
+            );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return response()->json(array(
+            'selected_branch_details' => $this->findBranch($id)
+        ),
+            200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $branch = $this->findBrand($id);
+
+        if(count($branch) > 0) {
+            $branch->str_branch_location     = $request->str_branch_location;
+
+            $branch->save();
+        }
+        return response()
+            ->json([
+                'message'       =>  'Branch is successfully updated.',
+                'branch'        =>  $branch
+            ],
+                200
+            );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function findBranch($id) {
+        return Branch::find($id);
+    }
+}
